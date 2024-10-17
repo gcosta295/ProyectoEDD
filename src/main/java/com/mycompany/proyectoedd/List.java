5556f7db546b1571d46e1a3fcee906290ccffdfd
astv06 <156257557+astv06@users.noreply.github.com>
2024-10-17 18:46:42 -0400

    clases de lista
    progreso listas 1

---
+++ src/main/java/com/mycompany/proyectoedd/List.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectoedd;
+
+/**
+ *
+ * @author astv06
+ */
+class List {
+    private String Lname;           //Name of the List
+    private Station SFirth;         //Firth of the List
+    private Station SLast;          //Last of the List 
+    private int Long;               //Length of the 
+
+    public List() {     
+        this.Lname = null;
+        this.Long = 0;
+        this.SFirth = null;
+        this.SLast = null;
+    }
+
+    public String getLname() {       
+        return Lname;
+    }
+
+    public void setLname(String Lname) {
+        this.Lname = Lname;
+    }
+    
+    public Station getSFirth() {    
+        return SFirth;
+    }
+
+    public Station getSLast() {     
+        return SLast;
+    }
+
+    public int getLong() {
+        return Long;
+    }
+
+    public void setSFirth(Station SFirth) {
+        this.SFirth = SFirth;
+    }
+
+    public void setSLast(Station SLast) {
+        this.SLast = SLast;
+    }
+
+    public void setLong(int Long) {
+        this.Long = Long;
+    }
+    
+    public List AddToList(List L){                          //Add a new station on the list
+        Station S = new Station("hola",L.getLname());       //Create a new station
+        if (L.getLong()==0){                                //If the list is empty the statios will be the firth
+            L.setSFirth(S);
+            L.setSLast(S);
+        }
+        else{
+            Station SAux = L.getSLast();
+            List LAux = SAux.getConections();
+            if (LAux == null){
+                LAux.setSFirth(SAux);
+                LAux.setSLast(S);
+            }
+            else{
+                LAux.setSLast(S);
+            }
+            SAux.setConections(LAux);
+            L.setSLast(S);                                  //If the list isn't empty the station will be thw new last
+        }
+        L.setLong(Long + 1);                                //Changes le long of the list
+        return L;
+    } 
+    /*
+    llamo a la variable auxiliar
+    busco las conexiones de la variable auxiliar
+    */
+    /*public List DeleteToList(List L){
+        
+        recibir lista
+        recibir estacion
+        comprobar si la estacion pertenece a la lista
+        comprobar si la lista no esta vacia
+        comprobar si la lista tiene mas de un elemento
+        de tener mad de un elemento 
+        parca cada uno de los 
+        
+    }*/
+}
---
+++ src/main/java/com/mycompany/proyectoedd/Station.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectoedd;
+
+/**
+ *
+ * @author astv06
+ */
+public class Station {
+    private String Sdata;
+    private String SList;
+    private List Conections;
+
+    public Station(String data, String SList) {
+        this.Sdata = data;
+        this.SList = SList;
+        this.Conections = new List();
+    }
+
+    public String getSdata() {
+        return Sdata;
+    }
+
+    public void setSdata(String data) {
+        this.Sdata = data;
+    }
+    
+    public List getConections() {
+        return Conections;
+    }
+    
+    public void setConections(List Conections) {
+        this.Conections = Conections;
+    }
+    
+    public String getSList() {
+        return SList;
+    }
+    
+    public void setSList(String SList) {
+        this.SList = SList;
+    }
+    
+    /*public Line last(Line Firth){
+        if (Firth.getLnext()==null){
+            return Firth;
+        }
+        else{
+            Line Laux = Firth.getLnext();
+            while (Laux.getLnext()!=null){
+                Laux = Laux.getLnext();
+            }
+            return Laux;
+        }
+    }*/
+    /*public Station Slast (Station Firth){
+        
+    }*/
+
+    
+
+    
+
+    
+}
