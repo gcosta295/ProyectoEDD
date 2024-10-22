e684a3730b3f21ee9764b3e39becbcd08500e2d5
gcosta295 <156736652+gcosta295@users.noreply.github.com>
2024-10-22 16:10:51 -0400

    agregar libreria graphstream

--- src/main/java/com/mycompany/proyectoedd/Linea1.java
+++ src/main/java/com/mycompany/proyectoedd/Linea1.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package com.mycompany.proyectoedd;
+import org.graphstream.graph.*; //importe de la libreria de graphstream
+
 
 /**
  *
