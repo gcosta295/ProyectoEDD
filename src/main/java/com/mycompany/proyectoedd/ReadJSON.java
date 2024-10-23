c84f6b3b08fd8325450ae800cd36bd1552e64e2a
gcosta295 <156736652+gcosta295@users.noreply.github.com>
2024-10-22 23:52:44 -0400

    validacion de JSON

--- src/main/java/com/mycompany/proyectoedd/ReadJSON.java
+++ src/main/java/com/mycompany/proyectoedd/ReadJSON.java
@@ -15,72 +15,75 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-
 /**
  *
  * @author gabri
  */
 public class ReadJSON {
+
     private String text;
     private String test;
+
     public ReadJSON() {
     }
-        
-    
-    public void abrirArchivo() {
-        String aux="";   
-        setText("");
-        try
-        {
-         /**llamamos el metodo que permite cargar la ventana*/
-         JFileChooser file=new JFileChooser();
-         file.showOpenDialog(file);
-         /**abrimos el archivo seleccionado*/
-         File abre=file.getSelectedFile();
 
-         /**recorremos el archivo, lo leemos para plasmarlo
-         *en el area de texto*/
-         if(abre!=null)
-         {     
-            FileReader archivos=new FileReader(abre);
-            BufferedReader lee =new BufferedReader(archivos);
-            while((aux=lee.readLine())!=null)
-            {
-                    setText(getText() + aux+ "\n");
+    
+
+    public void abrirArchivo() {
+        String aux = "";
+        setText("");
+        try {
+            //El metodo que nos permite abrir la ventana
+            JFileChooser file = new JFileChooser();
+            file.showOpenDialog(file);
+            File abre = file.getSelectedFile();
+            String fileType = file.getTypeDescription(abre); //tells us what type of file the chosen one is 
+            
+            
+            if (fileType.equals("JSON Source File")) { //Validates the JSON file
+                if (abre != null) {
+                    FileReader archivos = new FileReader(abre);
+                    BufferedReader lee = new BufferedReader(archivos);
+                    while ((aux = lee.readLine()) != null) {
+                        setText(getText() + aux+ "\n");
+                    }
+                    lee.close();
+                }
+            } else{
+                JOptionPane.showMessageDialog(null,
+                     "\nEl archivo no es JSON, por favor volver a intentar",
+                    "ADVERTENCIA!!!", JOptionPane.ERROR_MESSAGE);
             }
-               lee.close();
-          }    
-         }
-         catch(IOException ex)
-         {
-           JOptionPane.showMessageDialog(null,ex+"" +
-                 "\nNo se ha encontrado el archivo",
-                       "ADVERTENCIA!!!",JOptionPane.WARNING_MESSAGE);
-          }
-        
-      }
-    
-    
-        public void Parse() { // en teoria deberia recibir como parametro el grafo, pero falta crearlo
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null, ex + ""
+                    + "\nNo se ha encontrado el archivo",
+                    "ADVERTENCIA!!!", JOptionPane.WARNING_MESSAGE);
+        }
+
+    }
+
+    public void Parse() { // en teoria deberia recibir como parametro el grafo, pero falta crearlo
         String jsonString = text;
-               
 
         try {
             JSONParser parser = new JSONParser(); //creates a json parsser, to parse the json string
-            JSONArray linesArray = (JSONArray) parser.parse(jsonString); //the JSONArray allows to parse through the string
-            
-            for (Object lineObject : linesArray) { //iterates through each metroline in the array
+            JSONObject metroJson = (JSONObject) parser.parse(jsonString);
+            String nombreMetro = (String) metroJson.keySet().iterator().next(); //gets the name of the metro system
+            JSONArray lineasMetro = (JSONArray) metroJson.get(nombreMetro); //allows me to enter the metro system list of lines
+
+            for (Object lineObject : lineasMetro) { //iterates through each metroline in the array
                 JSONObject lineJson = (JSONObject) lineObject;
-                
+
                 for (Object lineNameObject : lineJson.keySet()) { //each metroline is transformed into an object
                     String lineName = (String) lineNameObject; //transform the metroline name into a string
-                    System.out.println("Linea: " + lineName);
-                    List metroline = new List(); // Creates a list of metroline, where each line will be stored 
+
+                    Line metroLine = new Line(lineName); //creates a new metroline, with the name of the line in the JSON
+
+                    List listaStations = new List(); // Creates a list of metroline, where the list of stations will be stored
                     // Create graph / here the metroline should be added
-                    metroline.setLname(lineName); //sets the name of the new metroline
-                   
+
                     JSONArray stationsArray = (JSONArray) lineJson.get(lineName); //creates an array of the metroline we are currently parsing
-                    
+
                     for (Object stationObject : stationsArray) { //creates an object of each station in the array, iterating over them
                         if (stationObject instanceof JSONObject) { //esto es lo de las estaciones con llaves, me falta arreglarlo
                             JSONObject connection = (JSONObject) stationObject;
@@ -89,11 +92,12 @@ public class ReadJSON {
                                 String connectionName = connection.get(stationNameObject).toString();
 //                                System.out.println("  " + stationName + " (Connection: " + connectionName + ")");
                             }
-                        } else { 
-                            String station = (String) stationObject; //gets the name of the station
-                            metroline.AddStationToList(metroline, station); //adds the iterated station into our own list class
-                            
-                            System.out.println("  " + station); 
+                        } else {
+                            String stationName = (String) stationObject; //gets the name of the station
+                            Station newStation = new Station(stationName);
+                            listaStations.AddStation(newStation); //adds the iterated station into our own list class
+
+                            System.out.println("  " + newStation.getsData());
                         }
                     }
                 }
@@ -102,7 +106,7 @@ public class ReadJSON {
             e.printStackTrace();
         }
     }
-    
+
     /**
      * @return the text
      */
@@ -116,6 +120,7 @@ public class ReadJSON {
     public void setText(String text) {
         this.text = text;
     }
+
     public String getTest() {
         return test;
     }
@@ -126,5 +131,5 @@ public class ReadJSON {
     public void setTest(String text) {
         this.text = test;
     }
-   
+
 }
