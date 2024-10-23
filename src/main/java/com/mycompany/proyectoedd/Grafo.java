2ede3465590e4bb9c3ba588ee129a91fe26a2202
nc-mirandaa <n.miranda@correo.unimet.edu.ve>
2024-10-23 18:11:40 -0400

    merge

--- src/main/java/com/mycompany/proyectoedd/Grafo.java
+++ src/main/java/com/mycompany/proyectoedd/Grafo.java
@@ -3,17 +3,60 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.proyectoedd;
-
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.*;
+import org.graphstream.graph.implementations.SingleGraph;
 /**
  *
  * @author astv06
  */
+        
 public class Grafo {
+
     
-    private List listStations;
+    /*private List listLines; //ignoramos esto
+    private String gName;
 
     public Grafo() {
-        this.listStations = new List();
+        this.listLines = new List();
+        this.gName = null;
     }
-    
+
+    public List getListLines() {
+        return listLines;
+    }
+
+    public String getgName() {
+        return gName;
+    }
+
+    public void setgName(String gName) {
+        this.gName = gName;
+    }
+     */
+    public void Grafo(List stations) {
+		System.setProperty("org.graphstream.ui", "swing");
+                Graph graph = new SingleGraph("Tutorial 1");
+		for (int i = 1; i <= stations.getlen(); i++) {
+                    Station x = stations.getStation(stations, i);
+                    Station x1 = stations.getStation(stations, i).getNext();
+                    if (i < stations.getlen()){  
+                        if (i==1){
+                            graph.addNode(x.getsData());
+                            graph.addNode(x1.getsData());
+                        }
+                        else{
+                            graph.addNode(x1.getsData());
+                        }
+                        if (x.getlData() == x1.getlData()){
+                            graph.addEdge(x.getsData()+x1.getsData(), x.getsData(), x1.getsData());
+                        }
+                    }
+                }
+		graph.display();
+
+				
+	}
+   
+
 }
--- src/main/java/com/mycompany/proyectoedd/ProyectoEDD.java
+++ src/main/java/com/mycompany/proyectoedd/ProyectoEDD.java
@@ -11,14 +11,29 @@ package com.mycompany.proyectoedd;
 public class ProyectoEDD {
 
     public static void main(String[] args) {
-        Station station = new Station("hola");
-        Line linea = new Line("line 1");
-        linea.getStations().AddStation(station);
-        Station station1 = new Station("chao");
-        linea.getStations().AddStation(station1);
         
-        station.conect(station1);
-        station1.conect(station);
-        System.out.println(linea.getStations().inList(station));
+        List stations = new List();
+        Line linea = new Line("line 1");
+        Line linea1 = new Line("line 2");
+        Station station = new Station("station0", linea.getLname());
+        Station station1 = new Station("station1", linea.getLname());
+        Station station2 = new Station("station2", linea.getLname());
+        Station station3 = new Station("station3", linea1.getLname());
+        Station station4 = new Station("station4", linea1.getLname());
+        stations.AddStation(station);
+        stations.AddStation(station1);
+        stations.AddStation(station2);
+        stations.AddStation(station3);
+        stations.AddStation(station4);
+        /*System.out.print(station.getsData() + " esta conectada con ");
+        System.out.println(station.getconections().getStation(station.getconections(), 1).getsData());
+        System.out.print(station1.getsData() + " esta conectada con ");
+        System.out.println(station1.getconections().getStation(station1.getconections(), 1).getsData());
+        System.out.println(stations.getLine(stations, 1).getLname());*/
+        
+        Grafo graf = new Grafo();
+        graf.Grafo(stations);
     }
+        
+    
 } 
