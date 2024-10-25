21265be3cc649c05c7fcb622c561968bd22df985
nc-mirandaa <n.miranda@correo.unimet.edu.ve>
2024-10-25 15:24:52 -0400

    Interfaz Añadir una Linea Nueva
    se logra añadir una nueva linea con sus estaciones y se añade a la lista de lineas

--- src/main/java/com/mycompany/proyectoedd/InterfazUnica.java
+++ src/main/java/com/mycompany/proyectoedd/InterfazUnica.java
@@ -14,8 +14,8 @@ import javax.swing.JOptionPane;
 public class InterfazUnica extends javax.swing.JFrame {
 
     ReadJSON L;
-    private static List listaLines1;
-    private static Grafo grapf1;
+    private List listaLines;
+    
     
     
     /**
@@ -26,6 +26,12 @@ public class InterfazUnica extends javax.swing.JFrame {
         initComponents();
     }
 
+    public void setListaLines(List listaLines) {
+        this.listaLines = listaLines;
+    }
+
+    
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -43,12 +49,20 @@ public class InterfazUnica extends javax.swing.JFrame {
         jPanel8 = new javax.swing.JPanel();
         B_carga_JSON = new javax.swing.JButton();
         jPanel9 = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
+        input_valorT = new javax.swing.JTextField();
+        B_changeT = new javax.swing.JButton();
         jPanel10 = new javax.swing.JPanel();
         input_nameLine = new javax.swing.JTextField();
         input_numberStation = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         B_addNewLine = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        Eliminar = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new javax.swing.BoxLayout(getContentPane(), javax.swing.BoxLayout.LINE_AXIS));
@@ -70,8 +84,11 @@ public class InterfazUnica extends javax.swing.JFrame {
             .addGap(0, 0, Short.MAX_VALUE)
         );
 
+        jTabbedPane2.setFont(new java.awt.Font("Monospaced", 0, 12)); // NOI18N
+
         jPanel8.setBackground(new java.awt.Color(204, 246, 242));
 
+        B_carga_JSON.setFont(new java.awt.Font("Monospaced", 0, 12)); // NOI18N
         B_carga_JSON.setText("Cagar JSON");
         B_carga_JSON.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -91,7 +108,7 @@ public class InterfazUnica extends javax.swing.JFrame {
         jPanel8Layout.setVerticalGroup(
             jPanel8Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel8Layout.createSequentialGroup()
-                .addContainerGap(156, Short.MAX_VALUE)
+                .addContainerGap(160, Short.MAX_VALUE)
                 .addComponent(B_carga_JSON, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(120, 120, 120))
         );
@@ -100,15 +117,38 @@ public class InterfazUnica extends javax.swing.JFrame {
 
         jPanel9.setBackground(new java.awt.Color(204, 255, 231));
 
+        jLabel3.setFont(new java.awt.Font("Monospaced", 0, 12)); // NOI18N
+        jLabel3.setText("Valor T :");
+
+        B_changeT.setFont(new java.awt.Font("Monospaced", 0, 12)); // NOI18N
+        B_changeT.setText("Cambiar");
+
         javax.swing.GroupLayout jPanel9Layout = new javax.swing.GroupLayout(jPanel9);
         jPanel9.setLayout(jPanel9Layout);
         jPanel9Layout.setHorizontalGroup(
             jPanel9Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 320, Short.MAX_VALUE)
+            .addGroup(jPanel9Layout.createSequentialGroup()
+                .addGroup(jPanel9Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel9Layout.createSequentialGroup()
+                        .addGap(27, 27, 27)
+                        .addComponent(jLabel3)
+                        .addGap(18, 18, 18)
+                        .addComponent(input_valorT, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel9Layout.createSequentialGroup()
+                        .addGap(97, 97, 97)
+                        .addComponent(B_changeT, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(88, Short.MAX_VALUE))
         );
         jPanel9Layout.setVerticalGroup(
             jPanel9Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 341, Short.MAX_VALUE)
+            .addGroup(jPanel9Layout.createSequentialGroup()
+                .addGap(144, 144, 144)
+                .addGroup(jPanel9Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(input_valorT, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addGap(52, 52, 52)
+                .addComponent(B_changeT, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(86, Short.MAX_VALUE))
         );
 
         jTabbedPane2.addTab("T", jPanel9);
@@ -166,12 +206,71 @@ public class InterfazUnica extends javax.swing.JFrame {
                 .addGroup(jPanel10Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(input_numberStation, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 52, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 56, Short.MAX_VALUE)
                 .addComponent(B_addNewLine, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(36, 36, 36))
         );
 
         jTabbedPane2.addTab("Línea", jPanel10);
+
+        jPanel2.setBackground(new java.awt.Color(204, 229, 255));
+
+        jButton1.setFont(new java.awt.Font("Monospaced", 0, 12)); // NOI18N
+        jButton1.setText("Añadir");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        Eliminar.setFont(new java.awt.Font("Monospaced", 0, 12)); // NOI18N
+        Eliminar.setText("Eliminar");
+        Eliminar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                EliminarActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setFont(new java.awt.Font("Monospaced", 0, 14)); // NOI18N
+        jLabel4.setText("Quieres añadir una sucursal");
+
+        jLabel5.setFont(new java.awt.Font("Monospaced", 0, 14)); // NOI18N
+        jLabel5.setText("Quieres eleminar una sucursal");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(43, 43, 43)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel5)
+                            .addComponent(jLabel4)))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(95, 95, 95)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(95, 95, 95)
+                        .addComponent(Eliminar, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(26, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(60, 60, 60)
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 60, Short.MAX_VALUE)
+                .addComponent(jLabel5)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(Eliminar, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(71, 71, 71))
+        );
+
+        jTabbedPane2.addTab("Sucursal", jPanel2);
 
         javax.swing.GroupLayout jPanel6Layout = new javax.swing.GroupLayout(jPanel6);
         jPanel6.setLayout(jPanel6Layout);
@@ -197,6 +296,11 @@ public class InterfazUnica extends javax.swing.JFrame {
         
         L.abrirArchivo();
         List listaLines = L.Parse();
+        List listL = this.listaLines;
+        this.listaLines = L.Parse();
+
+        System.out.println(this.listaLines.getLen());
+        
                 
         JOptionPane.showMessageDialog(this, "Se ha cargado el JSON");
         
@@ -211,7 +315,7 @@ public class InterfazUnica extends javax.swing.JFrame {
 
         List newlstations = new List();
         Line newline = new Line(lineName);
-        
+        System.out.println(this.listaLines.getlen());
         for (int i = 0; i < num; i++) {
             
             String stationName = JOptionPane.showInputDialog(this, "Escribe el nombre de la station: "); //agregar que cada nombre es una station (volver objects)
@@ -223,11 +327,24 @@ public class InterfazUnica extends javax.swing.JFrame {
              System.out.println(newline.getStations().getlen()); //l1.listastations.nodo
             
         }
-        listaLines.AddLine(newline); //ayuda dice que la lista es null //no esta tomando la lista vieja
-             System.out.println(listaLines.getlen());
+        System.out.println(this.listaLines.getlen());
+        this.listaLines.AddLine(newline); //ayuda dice que la lista es null //no esta tomando la lista vieja
+             System.out.println(this.listaLines.getlen());
         
         JOptionPane.showMessageDialog(this, "Se ha añadido una linea nueva");
     }//GEN-LAST:event_B_addNewLineActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        
+        String stationName = JOptionPane.showInputDialog(this, "Escribe el nombre de la station donde deseas colocar una sucursal: ");
+        String tbusqueda = JOptionPane.showInputDialog(this, "Que método quieres usar: DFS o BFS ");
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void EliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_EliminarActionPerformed
+        
+        String stationName = JOptionPane.showInputDialog(this, "Escribe el nombre de la station donde deseas eliminar una sucursal: ");
+        String tbusqueda = JOptionPane.showInputDialog(this, "Que método quieres usar: DFS o BFS ");
+    }//GEN-LAST:event_EliminarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -267,12 +384,20 @@ public class InterfazUnica extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton B_addNewLine;
     private javax.swing.JButton B_carga_JSON;
+    private javax.swing.JButton B_changeT;
+    private javax.swing.JButton Eliminar;
     private javax.swing.JTextField input_nameLine;
     private javax.swing.JTextField input_numberStation;
+    private javax.swing.JTextField input_valorT;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel10;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JPanel jPanel6;
     private javax.swing.JPanel jPanel7;
